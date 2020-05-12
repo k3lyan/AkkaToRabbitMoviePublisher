901e8b130bd6e691cfb9e2e4c3f9cd3a672364e2
k3lyan <kelyan.morfouesse@gmail.com>
2020-05-12 15:58:42 +0200

    UPD - Disable async to enhance performances

--- src/main/scala/AkkaToRabbit.scala
+++ src/main/scala/AkkaToRabbit.scala
@@ -8,6 +8,7 @@ import akka.stream.alpakka.amqp.scaladsl.AmqpSink
 import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 import akka.stream.scaladsl.{FileIO, Sink, Source}
 import akka.util.ByteString
+import com.typesafe.config.ConfigFactory
 import ProductRequirements.{fitWithProductRequirements, getTargetedValue}
 
 import scala.concurrent.Future
@@ -15,16 +16,16 @@ import scala.util.{Failure, Success}
 
 object AkkaToRabbit extends App {
   implicit val system = ActorSystem("AkkaToRabbit")
-  // implicit val actorSystem: ActorSystem[Nothing] = ActorSystem[Nothing](Behaviors.empty, "alpakka-samples")
   implicit val dispatcher = system.dispatchers.lookup("akka.actor.dedicated-dispatcher")
-
-  val sourceIO = FileIO.fromPath(Paths.get("src/main/resources/data/title.basics.tsv")).async
+  
+  val path: String = ConfigFactory.load().getString("input-data")
+  val sourceIO:String => Source[ByteString, Future[IOResult]] = (path: String) => FileIO.fromPath(Paths.get(path))
 
   def filterSource(source: Source[ByteString, Future[IOResult]]) = source
-    .via(CsvParsing.lineScanner(delimiter='\t')).async
-    .via(CsvToMap.toMapAsStrings()).async
-    .filter(fitWithProductRequirements).async
-    .map(getTargetedValue).async
+    .via(CsvParsing.lineScanner(delimiter='\t'))
+    .via(CsvToMap.toMapAsStrings())
+    .filter(fitWithProductRequirements)
+    .map(getTargetedValue)
 
   val connectionProvider = RabbitMQConnect.getConnection
   val queueName = RabbitMQConnect.RABBITMQ_QUEUE
@@ -37,7 +38,7 @@ object AkkaToRabbit extends App {
         .withDeclaration(queueDeclaration)
     )
 
-  val sinkWriter: Future[Done] = filterSource(sourceIO).runWith(amqpSink)
+  val sinkWriter: Future[Done] = filterSource(sourceIO(path)).runWith(amqpSink)
   sinkWriter.onComplete {
     case Success(done) =>
       println(s"$done")
